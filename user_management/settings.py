579023d6c73f6833d09ff98d7e9cbf710b3e1cc5
Ayush <parekh56@uwindsor.ca>
2025-01-29 10:42:53 -0500

    updated allowed hosts v2

--- user_management/settings.py
+++ user_management/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://workify-9i2l.onrender.com/", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
